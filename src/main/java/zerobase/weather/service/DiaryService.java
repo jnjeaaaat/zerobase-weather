4e7e73af283ae6037e8c877067bb355868ac2727
parktj <ekswn120@gmail.com>
2023-10-25 03:43:21 +0900

    openweather apiKey 추가

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,6 +1,7 @@
 package zerobase.weather.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import zerobase.weather.domain.Diary;
 import zerobase.weather.dto.DiaryDto;
@@ -13,7 +14,14 @@ import java.time.LocalDate;
 public class DiaryService {
     private final DiaryRepository diaryRepository;
 
+    @Value("${openweathermap.key}")
+    private String apiKey;
+
     public DiaryDto createDiary(LocalDate date, String text) {
         return null;
+    }
+
+    private String getWeatherString() {
+        "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
     }
 }
